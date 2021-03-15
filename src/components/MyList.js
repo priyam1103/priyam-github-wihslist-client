4286ed014f3a8331ca7d13a3afbb837f0e97344b
priyam1103 <priyampoddar89@gmail.com>
2021-03-16 04:20:05 +0530

    deployment changes

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -31,23 +31,25 @@ export default function MyList() {
     <div className="mylist-comp">
       {data.length > 0 ? (
         <div className="list">
-          {data.map((item) => (
-              <Card style={{ cursor: "pointer" }} onClick={()=>{history.push({pathname:`/gitprofile/${item.username}`,state:item});}}>
-              <Image src={item.avatar_url} wrapped ui={false} />
-              <Card.Content>
-                <Card.Header>{item.name}</Card.Header>
-                <Card.Meta>
-                  <span className="date">{item.username}</span>
-                </Card.Meta>
-                <Card.Description>{item.bio}</Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                
-                  <Icon name="user" />
-                  {item.public_repos} Public Repos
-                
-              </Card.Content>
-            </Card>
+          {data.map((item, index) => (
+            <>{item!==null &&  <Card style={{ cursor: "pointer" }} key={index} onClick={()=>{history.push({pathname:`/gitprofile/${item.username}`,state:item});}}>
+            <Image src={item.avatar_url} wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>{item.name}</Card.Header>
+              <Card.Meta>
+                <span className="date">{item.username}</span>
+              </Card.Meta>
+              <Card.Description>{item.bio}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              
+                <Icon name="user" />
+                {item.public_repos} Public Repos
+              
+            </Card.Content>
+          </Card>}</>
+            
+             
           ))}
         </div>
       ) :  <p className="center one-bold">
